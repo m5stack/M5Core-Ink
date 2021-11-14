c4eb0f1167e2176d07e589b084037f24c0bf385f
Tinyu <Tinyu.Zhao@gmail.com>
2021-11-14 22:20:11 +0800

    CoreInk Example

--- examples/RTC_BM8563/RTC_BM8563.ino
+++ examples/RTC_BM8563/RTC_BM8563.ino
@@ -1,6 +1,18 @@
+/*
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with CoreInk sample source code
+*                          配套  CoreInk 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/coreink
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/coreink
+*
+* describe：RTC example.  RTC示例
+* date：2021/11/14
+*******************************************************************************
+*/
 #include "M5CoreInk.h"
 
-Ink_Sprite InkPageSprite(&M5.M5Ink);
+Ink_Sprite InkPageSprite(&M5.M5Ink);  //创建 M5Ink实例
 
 RTC_TimeTypeDef RTCtime;
 RTC_DateTypeDef RTCDate;
@@ -8,46 +20,40 @@ RTC_DateTypeDef RTCDate;
 char timeStrbuff[64];
 
 void flushTime(){
-    M5.rtc.GetTime(&RTCtime);
-    M5.rtc.GetDate(&RTCDate);
-    
-    sprintf(timeStrbuff,"%d/%02d/%02d %02d:%02d:%02d",
-                        RTCDate.Year,RTCDate.Month,RTCDate.Date,
-                        RTCtime.Hours,RTCtime.Minutes,RTCtime.Seconds);
-                                         
-    InkPageSprite.drawString(10,100,timeStrbuff);
-    InkPageSprite.pushSprite();
+  M5.rtc.GetTime(&RTCtime); //Get RTC time. 获取RTC时间
+  M5.rtc.GetDate(&RTCDate); //Get RTC date. 获取RTC日期
+  sprintf(timeStrbuff,"%d/%02d/%02d %02d:%02d:%02d",
+                      RTCDate.Year,RTCDate.Month,RTCDate.Date,
+                      RTCtime.Hours,RTCtime.Minutes,RTCtime.Seconds);
+  InkPageSprite.drawString(10,100,timeStrbuff); //Draw time. 绘制时间
+  InkPageSprite.pushSprite(); //push the sprite. 推送图片
 }
 
 void setupTime(){
-  
   RTCtime.Hours = 23;
   RTCtime.Minutes = 33;
   RTCtime.Seconds = 33;
-  M5.rtc.SetTime(&RTCtime);
+  M5.rtc.SetTime(&RTCtime); //Set RTC time. 设置RTC时间
   
   RTCDate.Year = 2020;
   RTCDate.Month = 11;
   RTCDate.Date = 6;
-  M5.rtc.SetDate(&RTCDate);
+  M5.rtc.SetDate(&RTCDate); //Set RTC date. 设置RTC日期
 }
 
 void setup() {
-
-    M5.begin();
-    if( !M5.M5Ink.isInit())
-    {
-        Serial.printf("Ink Init faild");
-        while (1) delay(100);   
-    }
-    M5.M5Ink.clear();
-    delay(1000);
-    //creat ink refresh Sprite
-    if( InkPageSprite.creatSprite(0,0,200,200,true) != 0 )
-    {
-        Serial.printf("Ink Sprite creat faild");
-    }
-    setupTime();
+  M5.begin(); //Initialize CoreInk. 初始化 CoreInk
+  if( !M5.M5Ink.isInit()){  //check if the initialization is successful. 检查初始化是否成功
+    Serial.printf("Ink Init faild");
+    while (1) delay(100);   
+  }
+  M5.M5Ink.clear(); //clear the screen. 清屏
+  delay(1000);
+  //creat ink Sprite. 创建图像区域
+  if( InkPageSprite.creatSprite(0,0,200,200,true) != 0 ){
+    Serial.printf("Ink Sprite creat faild");
+  }
+  setupTime();
 }
 
 void loop() {
